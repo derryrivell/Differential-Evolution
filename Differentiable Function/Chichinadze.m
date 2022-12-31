e230ad1fdc4c965a40e8c633457e4ae1bf799606
derryromeo <121699462+derryromeo@users.noreply.github.com>
2022-12-31 07:51:31 +0700

    Delete Differentiable Function directory

--- Differentiable Function/Chichinadze.m
+++
@@ -1,2 +0,0 @@
-function y = Chichinadze(x)
-    y = x(1).^2-12*x(1)+8*sin(5*pi*x(1)/2)+10*cos(pi*x(1)/2)+11-(0.2*sqrt(5)/exp(0.5*((x(2)-0.5).^2)));
--- Differentiable Function/Keane.m
+++
@@ -1,2 +0,0 @@
-function y = Keane(x)
-     y=-((sin(x(1)-x(2))).^2)*((sin(x(1)+x(2))).^2)/sqrt(x(1).^2+x(2).^2);
